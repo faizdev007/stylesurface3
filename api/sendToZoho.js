28f2d60783fdff721f37ebaa4dd51af5e5c5d712
faizdev007 <faizdev007@gmail.com>
2025-12-04 00:02:10 +0530

    udpate

--- api/sendToZoho.js
+++ api/sendToZoho.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-    console.log("Received request:", req.method, req.body);
+    console.log("Received request:", req);
     return;
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method Not Allowed" });
