71a055fe45237df16170c01452cef54a2ddc1911
faizdev007 <faizdev007@gmail.com>
2025-12-04 13:16:43 +0530

    push

--- api/sendToZoho.js
+++ api/sendToZoho.js
@@ -44,6 +44,6 @@ export default async function handler(req, res) {
         return res.status(200).json({ success: true, data: zohoRes.data });
     } catch (err) {
         console.error(err.response?.data);
-        return res.status(500).json({ error: true, details: err.response?.data });
+        return res.status(404).json({ error: true, details: err.response?.data });
     }
 }
